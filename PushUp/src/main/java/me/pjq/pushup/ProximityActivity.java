483fb0cd65974c1a7e830eb074a4b32ea567f9d0
Jianqing.Peng <Jianqing.Peng@ef.com>
2013-11-08 11:52:38 +0800

    update gradle

---
+++ PushUp/src/main/java/me/pjq/pushup/ProximityActivity.java
@@ -0,0 +1,14 @@
+package me.pjq.pushup;
+
+import android.os.Bundle;
+
+/**
+ * Created by pengjianqing on 11/8/13.
+ */
+public class ProximityActivity extends BaseFragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+    }
+}
